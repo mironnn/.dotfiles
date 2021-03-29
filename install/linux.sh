dfa5da41765a8352573e252663a43925702b4dc7
Volodymyr Pashchenkov <volodymyr.pashchenkov@nextiva.com>
2021-03-29 13:19:34 +0300

    update debian apt list

--- install/linux.sh
+++ install/linux.sh
@@ -14,7 +14,6 @@ apt-get -qqy install cmake
 
 apt-get -qqy install software-properties-common
 apt-get -qqy install python-software-properties
-apt-get -qqy install neovim
 apt-get -qqy install python-dev
 apt-get -qqy install python-pip
 apt-get -qqy install python3-dev
@@ -32,6 +31,17 @@ pip3 install pynvim
 # mkdir -p ~/.local/share/fonts
 # cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
 # cd -
+touch /etc/apt/apt.conf.d/99defaultrelease && echo "APT::Default-Release "stable";" > /etc/apt/apt.conf.d/99defaultrelease
+cp ${SCRIPTPATH}/src/stable.list ${SCRIPTPATH}/src/testing.list /etc/apt/sources.list.d/
+
+apt update
+apt-get -qqy -t testing install neovim
+pip3 install -U setuptools
+pip3 install -U pynvim
+
+
+curl -sLf https://spacevim.org/install.sh | bash
+
 
 cd ${SCRIPTPATH}
 cp -R ./src/SpaceVim.d/ ~/.SpaceVim.d/
