2061a7c939b4f1674c59f12ca799fd270414ae04
Volodymyr Pashchenkov <volodymyr.pashchenkov@nextiva.com>
2021-03-29 15:23:55 +0300

    try tp split debian and ubuntu

--- install.sh
+++ install.sh
@@ -9,11 +9,50 @@ SCRIPTPATH=`pwd -P`
 
 printf "${CYAN}Installation for started\n${NORMAL}"
 
-if [ "$(uname)" = 'Linux' ]; then
-  printf "Platform Linux\n"
-  source install/linux.sh
+function get_os_and_version(){
+ 
+  if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+      # linuxbase.org
+      OS=$(lsb_release -si)
+      VER=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+      # For some versions of Debian/Ubuntu without lsb_release command
+      . /etc/lsb-release
+      OS=$DISTRIB_ID
+      VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+      # Older Debian/Ubuntu/etc.
+      OS=Debian
+      VER=$(cat /etc/debian_version)
+  elif [ -f /etc/SuSe-release ]; then
+      # Older SuSE/etc.
+      ...
+  elif [ -f /etc/redhat-release ]; then
+      # Older Red Hat, CentOS, etc.
+      ...
+  else
+      # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+      OS=$(uname -s)
+      VER=$(uname -r)
+  fi
+}
+
+get_os_and_version
+
+printf "OS: ${OS} ${VER}"
+
+if [ "$(OS)" = 'Debian' ]; then
+  printf "Platform Debian\n"
+  source install/debian.sh
+elif [ "$(OS)" = 'Ubuntu' ]; then
+  printf "Platform Ubuntu\n"
+  source install/ubuntu.sh
 elif [ "$(uname)" = 'Darwin' ]; then
   printf "Platform Darwin\n"
   source install/macos.sh
 fi
-
---
+++ install/ubuntu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+printf "${CYAN}Apt-get updating...${NORMAL}\n"
+apt -qq udpate
+apt install -y software-properties-common
+add-apt-repository ppa:neovim-ppa/unstable
+apt -qq update
+printf "${GREEN}DONE!${NORMAL}\n"
+
+
+printf "${CYAN}Install apps from apt...${NORMAL}\n"
+apt -qqy install python
+apt -qqy install python3
+apt -qqy install vim
+apt -qqy install tmux
+apt -qqy install cmake
+
+apt -qqy install software-properties-common
+apt -qqy install python-software-properties
+apt -qqy install python-dev
+apt -qqy install python-pip
+apt -qqy install python3-dev
+apt -qqy install python3-pip
+apt -qqy install silversearcher-ag
+
+apt -qqy install curl
+apt -qqy install xclip
+# apt -qqy install zsh
+apt -qqy install git-extras
+pip3 install --upgrade neovim
+
+pip3 install pynvim
+
+# mkdir -p ~/.local/share/fonts
+# cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+# cd -
+
+
+apt -qqy -t testing install neovim
+pip3 install -U setuptools
+pip3 install -U pynvim
+
+
+curl -sLf https://spacevim.org/install.sh | bash
+
+
+cd ${SCRIPTPATH}
+cp -R ./src/SpaceVim.d/ ~/.SpaceVim.d/
+
+printf "${GREEN}DONE!${NORMAL}\n"
